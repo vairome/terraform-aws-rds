3abe9cfe6af5dfdd11c1f242e09cb086ee71cc54
bmurillo <bmurillo@gbm.net>
2023-04-26 17:51:39 -0600

    Add the rds module

---
+++ rds.tf
@@ -0,0 +1,38 @@
+resource "aws_db_instance" "rds_instance" {
+  allocated_storage      = var.allocated_storage
+  identifier             = var.identifier
+  storage_type           = var.storage_type
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  port                   = var.port
+  username               = var.username
+  password               = var.password
+  db_name                = var.db_name
+  db_subnet_group_name   = var.db_subnet_group_name
+  publicly_accessible    = var.publicly_accessible
+  skip_final_snapshot    = var.skip_final_snapshot
+  vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
+
+}
+
+resource "aws_security_group" "rds_sg" {
+  name = "rds_sg"
+  vpc_id      = var.vpc_id
+
+  # Only Postgres in
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = var.private_subnets_eks_cidr
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
